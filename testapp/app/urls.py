ff1031f8a73fdeee1da3479eb4c1313efb54cb69
sivasoma-devops <sivasoma0806@gmail.com>
2026-01-23 15:37:36 +0530

    poll scm testing

--- testapp/app/urls.py
+++ testapp/app/urls.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.urls import path
 
 def index(request):
-    return HttpResponse("Hello, Namratha Garu! this is your new jenkins session! sivasoma")
+    return HttpResponse("Hello, Namratha Garu! this is your new jenkins session! sivasoma sankar narayana")
 
 urlpatterns = [
     path("", index),
